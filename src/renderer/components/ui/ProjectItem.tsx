6d42ed458a64a5d3f7336a0b41993189dcd00b4c
Kirill <traineratwot@yandex.ru>
2022-07-14 15:31:38 +0300

    1

--- src/renderer/components/ui/ProjectItem.tsx
+++ src/renderer/components/ui/ProjectItem.tsx
@@ -1,28 +1,38 @@
 import { useMemo, useState } from 'react';
+import rng                   from 'seedrandom';
 import { ProjectType }       from '../../../types/project';
 import '../../styles/projectItem.scss';
 
 const ProjectItem = (props: { project: ProjectType }) => {
+
 	const { project }       = props;
 	const [image, setImage] = useState('');
 	useMemo(() => {
+		const backgroundColors = [
+			'#F44336', '#E91E63', '#9C27B0',
+			'#673AB7', '#3F51B5', '#2196F3',
+			'#03A9F4', '#00BCD4', '#009688',
+			'#4CAF50', '#8BC34A', '#CDDC39',
+			'#FFEB3B', '#FFC107', '#FF9800',
+			'#FF5722', '#795548', '#607D8B'
+		];
 		if (project?.logo) {
 			setImage(project.logo);
 			return;
 		}
+		const color = backgroundColors[Math.floor(rng(project.name)() * (backgroundColors.length - 1))];
+		// const color = backgroundColors[random.int(0, backgroundColors.length)];
 		const n   = project.name.charAt(0).toUpperCase();
 		const img = `data:image/svg+xml;base64,${window.btoa(`
 <svg width='40' height='40' xmlns='http://www.w3.org/2000/svg'>
 	<g>
-		<ellipse stroke='#000' ry='18' rx='18' id='svg_1' cy='20' cx='20' fill='#fff'/>
-		<text font-weight='bold' font-family='Fira Code' text-anchor='middle' dominant-baseline='middle' font-size='24' stroke-width='0' id='svg_2' y='22.5' x='20' stroke='#000' fill='#000000'>${n}</text>
+		<ellipse stroke='#000' ry='18' rx='18' id='svg_1' cy='20' cx='20' fill='${color}'/>
+		<text font-weight='bold' font-family='Fira Code' text-anchor='middle' dominant-baseline='middle' font-size='24' stroke-width='0' id='svg_2' y='22.5' x='20' stroke='#000' fill='#fff'>${n}</text>
 	</g>
 </svg>
 		`.trim())}`;
 		setImage(img);
-	}, [
-				project
-			]);
+	}, [project.logo, project.name]);
 
 
 	return (
