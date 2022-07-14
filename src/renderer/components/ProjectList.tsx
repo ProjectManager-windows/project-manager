6c2a994aba5253f36dfeee5e19c489b7a4831159
Kirill <traineratwot@yandex.ru>
2022-07-14 12:14:10 +0300

    1

--- src/renderer/components/ProjectList.tsx
+++ src/renderer/components/ProjectList.tsx
@@ -3,12 +3,19 @@ import ProjectItem from './ui/ProjectItem';
 
 const ProjectList = () => {
 	const projects = window.electron.projects.getAll();
+	let forSort  = [];
 	const list     = [];
 
 	// eslint-disable-next-line guard-for-in
 	for (const projectsKey in projects) {
 		const project = projects[projectsKey];
-		list.push(<ProjectItem key={projects.id} project={project}/>);
+		forSort.push(project);
+	}
+	forSort = forSort.sort((a, b) => {
+		return a.name.localeCompare(b.name)
+	});
+	for (const project of forSort) {
+		list.push(<ProjectItem key={projects.id} project={project} />);
 	}
 	return (
 		<div className='projectList'>
