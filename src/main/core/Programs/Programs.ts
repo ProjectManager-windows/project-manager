71d2a3b7682ae227c47f3102a1c184d6f2d8a1f1
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-10-29 21:53:38 +0300

    Update Programs.ts

--- src/main/core/Programs/Programs.ts
+++ src/main/core/Programs/Programs.ts
@@ -2,8 +2,6 @@ import PM_Storage, { Tables }                  from '../Storage/PM_Storage';
 import { Program, ProgramFields, ProgramType } from './Program';
 import { ipcMain }                             from 'electron';
 import { BackgroundEvens }                     from '../../../utills/Enums';
-import Settings                                from '../Settings';
-import Projects                                from '../Projects/Projects';
 
 export class Programs {
 	public programsData: { [p: string]: ProgramFields } | undefined;
@@ -16,22 +14,6 @@ export class Programs {
 		ipcMain.on(BackgroundEvens.ProgramsGetProject, async (event, id) => {
 			event.returnValue = Program.fromId(id);
 		});
-		ipcMain.on(BackgroundEvens.ProgramsExecute, async (_event, projectId) => {
-			const defaultIde = Number(Settings.get('defaultIde'));
-			const project    = Projects.getById(projectId);
-			let ideId: number;
-			if (project.getVal('ide')) {
-				ideId = project.getVal('ide');
-			} else {
-				ideId = defaultIde || 1;
-			}
-			const ide          = this.getById(ideId);
-			_event.returnValue = await ide.execute(project);
-		});
-	}
-
-	getById(id: number) {
-		return Program.fromId(id);
 	}
 
 	getPrograms(type?: ProgramType) {
