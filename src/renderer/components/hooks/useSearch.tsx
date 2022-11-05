2bcd89b1a22b6951b394760ad925d67b002b5d5a
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-11-05 19:22:05 +0300

    Update useSearch.tsx

--- src/renderer/components/hooks/useSearch.tsx
+++ src/renderer/components/hooks/useSearch.tsx
@@ -2,22 +2,38 @@ import { useMemo }     from 'react';
 import * as JsSearch   from 'js-search';
 import { ProjectType } from '../../../types/project';
 
-export const useSearch = (props: { projects: { [key: string]: ProjectType }, searchString: string }):ProjectType[] => {
+export const useSearch = (props: { projects: { [key: string]: ProjectType }, searchString: string }): ProjectType[] => {
 	const { projects, searchString } = props;
-	const projectList                = Object.values(projects);
+	const projectList                = useMemo(() => {
+		return Object.values(projects).map(e => {
+			if (e.stats) {
+				// @ts-ignore
+				e._stats = Object.keys(e.stats).map((l) => {
+					return window.LanguagesExtensions.find((el) => {
+						return el?.extensions?.includes(l);
+					})?.name.toLowerCase();
+				}).filter((v) => !!v);
+			}
+			return e;
+		});
+	}, [projects]);
+
 
 	const search = useMemo(() => {
 		const search = new JsSearch.Search('id');
 		search.addIndex('name');
 		search.addIndex('path');
 		search.addIndex('description');
+		search.addIndex('_stats');
 		search.addDocuments(projectList);
 		return search;
 	}, [projectList]);
 
 	return useMemo(() => {
 		if (searchString) {
-			return search.search(searchString) as ProjectType[];
+			const a =search.search(searchString) as ProjectType[];
+			console.log(a);
+			return a
 		}
 		return projectList;
 	}, [projectList, search, searchString]);
